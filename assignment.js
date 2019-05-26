55966aa0c536a9803742b708f54c2edc7ee78fa2
Anastasiia Nesterenko <anesterenko@Anastasiias-MacBook-Air.local>
2019-05-26 13:10:10 -0400

    Added creation of Department table

--- assignment.js
+++ assignment.js
@@ -10,9 +10,26 @@ db.serialize(function(){
     db.run("INSERT INTO Classroom VALUES(10104, 'Watson', 100, 30)");
     db.run("INSERT INTO Classroom VALUES(10105, 'Watson', 120, 50)");
 
+    db.run("CREATE TABLE Department (ID NUMBER, Dept_name TEXT, Building TEXT, Budget NUMBER)");
+    db.run("INSERT INTO Department VALUES(10001, 'Biology', 'Watson', 90000)");
+    db.run("INSERT INTO Department VALUES(10001, 'Comp. Sci.', 'Taylor', 100000)");
+    db.run("INSERT INTO Department VALUES(10001, 'Elec. Eng.', 'Taylor', 85000)");
+    db.run("INSERT INTO Department VALUES(10001, 'Finance', 'Painter', 120000)");
+    db.run("INSERT INTO Department VALUES(10001, 'History', 'Painter', 50000)");
+    db.run("INSERT INTO Department VALUES(10001, 'Music', 'Packard', 80000)");
+    db.run("INSERT INTO Department VALUES(10001, 'Physics', 'Watson', 90000)");
+
     db.each("SELECT * FROM Classroom", function(err, row){
         if (err)
             console.log(err);
         console.log(row);
     });
+
+    db.each("SELECT * FROM Department", function(err, row){
+        if (err)
+            console.log(err);
+        console.log(row);
+    });
+
+
 })
